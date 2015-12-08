95ff249c0366eb032676d131a9bba6f3438b7c1c
Taf <stag0r@ukr.net>
2015-12-08 16:30:46 +0200

    Finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -56,3 +58,4 @@ Rails.application.routes.draw do
   #     resources :products
   #   end
 end
+
